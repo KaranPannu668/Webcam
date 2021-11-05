d422136de2d8c9ed3b361a35ef38b6e9b983bb8b
Akshdeep792 <76034766+Akshdeep792@users.noreply.github.com>
2021-11-05 16:41:51 -0400

    DB add

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const Datastore = require("nedb");
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+
 app.get("/", (req,res) =>{
     res.render("webcam");
+})
+
+app.post("/", (req,res) => {
+    const data = req.body;
+    database.insert(data);
+    res.json(data);
 })
 
 
